a4c47ee7cecb300c69b042f3dafa9b3b0ac0f7fb
MD.Kawsar Hasan <kawsarsf21@gmail.com>
2026-07-11 20:42:03 +0600

    Add files via upload

---
+++ BeeCrowd/Beginner/1041.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float X,Y;
+    cin>>X>>Y;
+    if(X>0&&Y>0)cout<<"Q1"<<endl;
+    else if(X<0&&Y>0)cout<<"Q2"<<endl;
+    else if(X<0&&Y<0)cout<<"Q3"<<endl;
+    else if(Y<0&&X>0)cout<<"Q4"<<endl;
+    else if(X==0&&Y!=0)cout<<"Eixo Y"<<endl;
+    else if(Y==0&&X!=0)cout<<"Eixo X"<<endl;
+    else cout<<"Origem"<<endl;
+    return 0;
+}
