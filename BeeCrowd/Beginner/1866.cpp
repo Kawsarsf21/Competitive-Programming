5088587ecf2d7dbbe5935da57e2dc8004a1bf8fc
MD.Kawsar Hasan <kawsarsf21@gmail.com>
2026-07-12 17:10:42 +0600

    Add files via upload

---
+++ BeeCrowd/Beginner/1866.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int S;
+    cin>>S;
+        for(int i=0;i<S;i++)
+        {
+            int N=0;
+            cin>>N;
+        if(N%2==0)cout<<"0"<<endl;
+        else cout<<"1"<<endl;
+        }
+    return 0;
+}
