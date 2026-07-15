cd43e3375e7161912e1eb4d8c26990a14123de10
MD.Kawsar Hasan <kawsarsf21@gmail.com>
2026-07-15 17:52:36 +0600

    Add files via upload

---
+++ BeeCrowd/Beginner/1864.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin>>N;
+    char S[]="LIFE IS NOT A PROBLEM TO BE SOLVED BUT A REALITY TO BE EXPERIENCED";
+    for(int i=0;i<N;i++)
+    {
+        cout<<S[i];
+    }
+    cout<<endl;
+    return 0;
+}
